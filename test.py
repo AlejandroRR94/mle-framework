c78da4002dd469391336d8612c10df1c81122a0a
AlejandroRR94 <rivasredondo94@gmail.com>
2023-07-10 15:37:30 +0200

    fixed tests

---
+++ test.py
@@ -0,0 +1,45 @@
+import pytest
+import joblib
+import pickle
+import xgboost as xgb
+import numpy as np
+from test_utils import get_last_model
+from sklearn.metrics import r2_score, mean_absolute_percentage_error
+
+class TestClass:
+
+    @pytest.fixture
+    def model(self):
+        return joblib.load(get_last_model("models", "pkl"))
+
+    @pytest.fixture
+    def X_test(self):
+        return joblib.load("data/test/test_attributes.pkl")
+
+
+    @pytest.fixture
+    def y_test(self):
+        return joblib.load("data/test/test_target.pkl")
+
+    @pytest.fixture
+    def y_pred(self, model, X_test):
+        return model.predict(X_test)
+
+    def test_r2_score(self, y_test, y_pred):
+        try: 
+            assert r2_score(y_true = y_test, y_pred = y_pred) >= 0.9
+            
+        except AssertionError as msg:
+            print("R2 Score < 0.9!\n")
+            print(msg)
+            
+
+    def test_mean_absolute_percentage_error(self, y_test, y_pred):
+        try:
+            assert mean_absolute_percentage_error(y_true = y_test, y_pred =y_pred) <= 0.15
+        except AssertionError as msg:
+            print("MAPE > 0.15!\n")
+            print(msg)
+
+        
+
--- test_utils.py
+++ test_utils.py
@@ -5,6 +5,7 @@ import xgboost as xgb
 import joblib
 import numpy as np
 from sklearn.metrics import r2_score, mean_absolute_percentage_error
+import pytest
 
 def get_last_model(path:str=None, format:str="pkl"):
     """
@@ -21,39 +22,54 @@ def get_last_model(path:str=None, format:str="pkl"):
     
     return file_dict[min(file_dict.keys())]
 
-    
+# @pytest.fixture
 def load_model(filename:str):
         
     """
     Cargamos el modelo
     """
     
-    xgb_model = joblib.load(filename)
+    model = joblib.load(filename)
 
-    return xgb_model
+    return model
+
+# @pytest.fixture
+def load_test_data():
+    try:
+        X_test = joblib.load("data/test/test_attributes.pkl")
+        y_test = joblib.load("data/test/test_target.pkl")
+
+        return X_test, y_test
+
+    except Exception as e:
+        print("Could not load the data attributes and target", e)
 
 
-def check_r2_score( y_real:np.array, y_predicted:np.array, thresh:float=0.9):   
+def test_r2_score(load_model, X_test, y_test:np.array, thresh:float=0.9):   
     
     """
     Comprueba que el coeficiente de regresión
     sea igual o superior a 0.9
     """
-
-    r2 = r2_score(y_true = y_real, y_pred = y_predicted)
+    
+    y_predicted = load_model.predict(X_test)
+    r2 = r2_score(y_true = y_test, y_pred = y_predicted)
 
     try:
         assert r2 >= thresh
     except AssertionError as msg:
      print(msg)
 
-def check_mean_percentage_error(y_real:np.array, y_predicted:np.array,thresh:float=0.15):
+
+def test_mean_percentage_error(load_model, X_test, y_test,thresh:float=0.15):
     """
     Comprueba que el error porcentual medio absoluto
     sea igual o menor al 15%
     """
+    
+    y_predicted = load_model.predict(X_test)
 
-    mpe = mean_absolute_percentage_error(y_true = y_real, y_pred = y_predicted)
+    mpe = mean_absolute_percentage_error(y_true = y_test, y_pred = y_predicted)
 
     try:
         assert mpe<=thresh
